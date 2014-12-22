a6103270442b1b1213eaa01b328e0e0bd4e5866b
Christian Manuel <ctmanuel@wpi.edu>
2014-12-22 05:11:39 -0500

    Added a text field and button to display message
    When the button is pressed, it creates a new Activity that displays the
    message that was input. This will be expanded to create phone
    notifications for "proof-of-concept"

--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -4,9 +4,14 @@ import com.example.helloworld.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,19 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Create a new intent that in turn will call the new activity to display the message
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	/*An Intent can carry data types as key-value pairs called extras. 
+    	 *The putExtra() method takes the key name in the first parameter and the value in the second
+    	 */
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);		//start the activity
     }
     
 }
