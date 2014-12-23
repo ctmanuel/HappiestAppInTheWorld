bfb668543d90a0953534433dfb346023b0333a99
Robert Dabrowski <rpdabrowski@wpi.edu>
2014-12-22 20:25:07 -0500

    added the settings page
    Added the settings/preferences page. Android API words it as
    preferences, but normal people say settings I guess?
    
    Anyways, add to xml\preferences.xml to add more preferences.

--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -1,43 +1,59 @@
 package com.example.helloworld;
 
-import com.example.helloworld.R;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import com.example.settings.SettingsActivity;
+
 public class MainActivity extends Activity {
 
-    public final static String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
+	public final static String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        // Create a new intent that in turn will call the new activity to display the message
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	/*An Intent can carry data types as key-value pairs called extras. 
-    	 *The putExtra() method takes the key name in the first parameter and the value in the second
-    	 */
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);		//start the activity
-    }
-    
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			Intent intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+		// Create a new intent that in turn will call the new activity to
+		// display the message
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		/*
+		 * An Intent can carry data types as key-value pairs called extras.The
+		 * putExtra() method takes the key name in the first parameter and the
+		 * value in the second
+		 */
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent); // start the activity
+	}
+
 }
---
+++ HelloWorld/src/com/example/settings/SettingsFragment.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.example.settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.example.helloworld.R;
+
+public class SettingsFragment extends PreferenceFragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.preferences);
+	}
+
+}
